6af0e05bb50b3c6c6e0f3fcf86462f3ecc3a60f6
Orkhan <jkrogers222@gmail.com>
2021-08-14 16:37:39 -0400

    restAssured prettyPrint

---
+++ src/test/java/com/cybertek/day1/SimpleGetRequest.java
@@ -0,0 +1,28 @@
+package com.cybertek.day1;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class SimpleGetRequest {
+
+    String url = "http://3.86.27.247:8000/api/spartans";
+
+    @Test
+    public void test1() {
+
+        Response response = RestAssured.get(url);
+        System.out.println(response.statusCode());
+
+        //print response body
+        response.prettyPrint();
+
+
+
+
+
+
+
+    }
+
+}
