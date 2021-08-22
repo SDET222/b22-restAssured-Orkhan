789483e83dd8edaff6a18b8cdec2988fbdb3cecb
Orkhan <jkrogers222@gmail.com>
2021-08-22 11:57:06 -0400

    list of spartans

--- src/test/java/com/cybertek/day6/SpartanPojoGetRequestTest.java
+++ src/test/java/com/cybertek/day6/SpartanPojoGetRequestTest.java
@@ -1,5 +1,6 @@
 package com.cybertek.day6;
 
+import com.cybertek.pojo.Search;
 import com.cybertek.pojo.Spartan;
 import com.cybertek.utilities.SpartanTestBase;
 import io.restassured.http.ContentType;
@@ -7,6 +8,8 @@ import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
 
@@ -49,5 +52,39 @@ public class SpartanPojoGetRequestTest extends SpartanTestBase {
         System.out.println("s1.getName() = " + s1.getName());
         System.out.println("s1.getGender() = " + s1.getGender());
 
+
     }
+    @Test
+    public void test3(){
+        Response response = given().accept(ContentType.JSON)
+                .and().queryParams("nameContains", "a",
+                        "gender", "Male")
+                .when().get("/api/spartans/search")
+                .then().statusCode(200)
+                .extract().response();
+
+        Search searchResult = response.as(Search.class);
+
+        System.out.println(searchResult.getContent().get(0).getName());
+    }
+    @DisplayName("GET  /spartans/search and save as List<Spartan>")
+    @Test
+    public void test4(){
+        List<Spartan> spartanList = given()
+                .accept(ContentType.JSON)
+                .and()
+                .queryParams("nameContains", "a",
+                        "gender", "Male")
+                .when()
+                .get("/api/spartans/search")
+                .then()
+                .statusCode(200)
+                .extract().jsonPath().getList("content", Spartan.class);
+
+        System.out.println(spartanList.get(1).getName());
+
+    }
+
+
+
 }
